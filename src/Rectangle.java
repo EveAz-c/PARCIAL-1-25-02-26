fb5aa7172ecaff0ebfca95caddd5e23578a61c4e
Evelyn perez uscanga <zs24016727@estudiantes.uv.mx>
2026-02-25 17:54:03 -0600

    Refactor Rectangle class to extend GeometricObject

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+ublic class Rectangle extends GeometricObject {
+    private double width;
+    private double height;
 
-public class Rectangle {
+    public Rectangle() {
+        super();
+        this.width = 1.0;
+        this.height = 1.0;
+    }
+    public Rectangle(double width, double height, String color, boolean filled) {
+        super(color, filled);
+        this.width = width;
+        this.height = height;
+    }
+    public double getWidth() {
+        return width;
+    }
+    public void setWidth(double width) {
+        this.width = width;
+    }
+    public  double getHeight(){
+        return height;
+    }
+    public void setHeight(double height) {
+        this.height = height;
+    }
+    public double getArea() {
+        return width * height;
+    }
+    public double getPerimeter() {
+        return 2 * (width + height);
+    }
+
 }
