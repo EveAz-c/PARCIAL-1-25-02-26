a12b837c75f7506d6ef867f3803d86a6c65fe2e5
Evelyn perez uscanga <zs24016727@estudiantes.uv.mx>
2026-02-25 17:52:49 -0600

    Update GeometricObject.java

--- src/GeometricObject.java
+++ src/GeometricObject.java
@@ -1,4 +1,41 @@
-package PACKAGE_NAME;
-
+import java.util.Date;
 public class GeometricObject {
+    protected String color;
+    protected boolean filled;
+    protected Date dateCreated;
+
+    public GeometricObject() {
+        this.color = "white";
+        this.filled = false;
+        this.dateCreated = new Date();
+    }
+    public GeometricObject(String color, boolean filled) {
+        this.color = color;
+        this.filled = filled;
+        this.dateCreated = new Date();
+    }
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    @Override
+    public String toString() {
+        return "creado el " + dateCreated + "\ncolor: " + color + " y relleno: " + filled;
+    }
 }
