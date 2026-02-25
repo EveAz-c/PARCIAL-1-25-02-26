64bdd0f63938c6588b50ba201ff9237344f858dd
Evelyn perez uscanga <zs24016727@estudiantes.uv.mx>
2026-02-25 17:52:27 -0600

    Update Circle.java

--- src/Circle.java
+++ src/Circle.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+public class Circle extends GeometricObject {
+    private double radio;
 
-public class Circle {
+    public Circle(){
+        super();
+        this.radio = 1.0;
+    }
+    public Circle(double radio, String color, boolean filled) {
+        super(color, filled);
+        this.radio = radio;
+    }
+    public double getRadio() {
+        return radio;
+    }
+    public void setRadio(double radio) {
+        this.radio = radio;
+}
+    public double getArea() {
+        return Math.PI * radio * radio;
+    }
+    public double getPerimeter() {
+        return 2 * Math.PI * radio;
+    }
+    public double getDiameter() {
+        return 2 * radio;
+    }
+    public void printCircle() {
+        System.out.println("Radio del circulo:" + radio);
+    }
 }
